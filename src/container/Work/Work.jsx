830cffb3912fadc3db4b7ced970096d3868c52d0
Nitish Singh <singhnitish198@gmail.com>
2022-07-02 03:15:01 +0530

    Added Backend to this repo only

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -43,7 +43,7 @@ const Work = () => {
 			</h2>
 
 			<div className="app__work-filter">
-				{["UI/UX", "Web App", "Mobile App", "React JS", "All"].map(
+				{["UI/UX", "Web App", "Flutter", "React JS", "All"].map(
 					(item, index) => (
 						<div
 							key={index}
